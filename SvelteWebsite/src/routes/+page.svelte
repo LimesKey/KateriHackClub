6a6c0abea20e9dae5d6043ab88905af44aca4d63
LimesKey <talk@limeskey.com>
2024-04-24 13:16:35 -0400

    update svelte website example

---
+++ SvelteWebsite/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<p> A simple Svelte Website for the St Kateri Hack Club...</p>
+
+<h1> Big Website!!</h1>
+<h2> Small Website</h2>
+<h3> Extra Small</h3>
+<title>Title!</title>
